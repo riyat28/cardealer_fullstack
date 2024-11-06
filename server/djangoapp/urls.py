4d935a09702c4978bccd9b6c72286fe6e35ea18b
riyat28 <riya.tambe28@gmail.com>
2024-11-06 08:17:01 -0500

    Initial commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,20 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+path('register/', views.User, name='register'),
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+     path(route='login', view=views.login_user, name='login'),
     # path for dealer reviews view
 
     # path for add a review view
+
+    # Add the logout path
+    path('logout/', views.login_user, name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
